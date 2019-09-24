0913f6db95c54af46e42d7138aa31a071159b881
sig4 <sig4@njit.edu>
2019-09-24 10:45:10 -0400

    rename initDb added sql folder

---
+++ sql/1_create_table_testusers.sql
@@ -0,0 +1,7 @@
+create table if not exists `TestUsers`(
+		`id int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		`PRIMARY KEY (`ID`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci
+
